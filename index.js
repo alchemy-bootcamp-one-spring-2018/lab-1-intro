06234791d22b35d8497f011f54e0a239d5de4458
Zachar13y <zachar13y@gmail.com>
2018-04-30 16:27:03 -0700

    Added four questions to a questionaire, and console logged responses.

---
+++ index.js
@@ -0,0 +1,34 @@
+const userName = prompt('What is your name?');
+console.log('User\'s name is', userName);
+
+const savedName = userName;
+alert('Hi ' + savedName + '. Welcome to the questionaire!');
+
+// *******This part is not working properly{
+let affirmative = ('Yes');
+const likesCoffee = (affirmative);
+prompt('Do you like coffee?');
+
+if(likesCoffee){
+    alert('Awesome! It\'s one of my favorite things.');
+    console.log(savedName + ' likes coffee.');
+}
+else {
+    alert('Maybe we shouldn\'t talk anymore.');
+}
+// ******This part is not working properly}
+
+const favoriteFood = prompt('What is your favorite food?');
+console.log(savedName + '\'s favorite food is', favoriteFood + '.');
+
+alert ('Nice ' + savedName + '! I like ' + favoriteFood + ' too!');
+
+var likesCars = confirm('Do you like cars? Click "OK" for Yes, or "Cancel" for no.');
+console.log(savedName + ' likes cars.');
+
+if(likesCars){
+    alert('That\'s great! Me too!');
+}
+else {
+    alert('That\'s not good. Stop eating ' + favoriteFood + ', and that will change.');
+}
